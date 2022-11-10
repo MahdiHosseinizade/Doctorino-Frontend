896fdcac7e523a25c5d6d4b15359bfce0333cf50
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2022-11-10 15:58:32 +0330

    ToastContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import routes from './routes';
 import { Switch,Route, BrowserRouter } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
+        <ToastContainer />
         <BrowserRouter>
         <Switch>
           {routes.map((route ) => (
