9b9a166fe8f500f5a78b1db898c5450cc52600e5
houmanhaghi <rezahaghighatgou@gmail.com>
2022-12-06 17:33:30 +0330

    schedule table added

---
+++ src/components/Doctor/DoctorProfile/scheduleTable.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    Container,
+    Card,
+
+} from "@mui/material"
+
+
+
+
+export default function ScheduleTime() {
+    return (
+        <Container>
+            <Card>
+                
+            </Card>
+        </Container>
+    );
+}
