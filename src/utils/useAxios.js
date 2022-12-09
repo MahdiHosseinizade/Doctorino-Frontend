3d484df90a1a58bd42672a9493eca86797a861ca
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2022-12-09 23:15:48 +0330

    DoctorSwipper

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -5,7 +5,7 @@ import { useContext } from 'react'
 import AuthContext from '../context/AuthContext'
 
 // const baseURL = 'http://127.0.0.1:8000'
-const baseURL = 'http://188.121.113.74'
+export const baseURL = 'http://188.121.113.74'
 
 
 const useAxios = () => {
