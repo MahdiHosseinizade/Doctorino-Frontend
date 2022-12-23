6665e1230a091bcbad87b5cf0feb8199219752e1
houmanhaghi <rezahaghighatgou@gmail.com>
2022-12-23 18:59:19 +0330

    cssk

--- src/components/Hotel/HotelList/Hotels.jsx
+++ src/components/Hotel/HotelList/Hotels.jsx
@@ -5,6 +5,36 @@ import HotelCard from './components/HotelCard';
 import { baseURL } from '../../../utils/useAxios';
 
 function Hotels(prop) {
+    // const result = ages.filter(checkAdult);
+    // const props = prop.copy();
+    
+
+    // const props = structuredClone(prop);
+    // let  props = JSON.parse(JSON.stringify(prop))
+    // console.log(props.map(item => ( item = [2]
+    //     // console.log(item.city)
+        
+       
+    // )))
+
+
+    // const currentCar = this.props.cars.find((car) => car.carId === currentId)
+    // console.log(prop.city.find((city) => city != null))
+    
+    // const kir = [1,2,3]
+    // const res = kir.filter(cos);
+    // console.log(res)
+    // function cos(){
+    //     return true
+    // }
+
+    // const ages = [32, 33, 16, 40];
+    // const result = prop.filter(checkAdult);
+
+    // function checkAdult(age) {
+    //     return age == 18;
+    // }
+
     const [hotels, setHotels] = useState([]);
 
     useEffect(() => {
@@ -16,8 +46,9 @@ function Hotels(prop) {
         let response = await axios.get(`${baseURL}/api/hotel/`)
 
         if (response.status === 200) {
+
             setHotels(response.data)
-            console.log(response.data)
+            response.data.map(item => console.log(item.city))
         } else {
             alert("Something went wrong")
         }
