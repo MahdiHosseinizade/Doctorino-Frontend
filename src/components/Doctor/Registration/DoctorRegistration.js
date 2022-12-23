5c394236a2117ae12a1cb5138e4f5e4ad126a1bb
mahdi <realmahdighazavi@gmail.com>
2022-12-23 19:44:58 +0330

    debugged-before-review

--- src/components/Doctor/Registration/DoctorRegistration.js
+++ src/components/Doctor/Registration/DoctorRegistration.js
@@ -59,6 +59,8 @@ const DoctorRegister = () => {
   const [doctorScale, setDoctorScale] = useState(null);
   const [error, setError] = useState("Ooops !!!!");
 
+  // var profilePicture = <img src="/src/assets/img/DoctorProfilePhoto.jpg" alt="Doctor"/>;
+  
   const history = useHistory();
   useEffect(() => {
     getdoctorScale();
@@ -103,6 +105,8 @@ const DoctorRegister = () => {
       },
       specialties: [parseFloat(doctor.scale)],
       medical_system_number: doctor.code.toString(),
+      // image: URL("../../../assets/img/DoctorProfilePhoto.jpg"),
+      // image: profilePicture,
       is_doctor: true,
     };
     postDoctorRegister(user)
