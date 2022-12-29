2d7b13d94044a707ab16bde22154c14a1a8c2174
mahdi <realmahdighazavi@gmail.com>
2022-12-30 02:48:04 +0330

    hotel-search-and-filter

--- src/components/LandingPage/HotelSearch/HotelSearch.jsx
+++ src/components/LandingPage/HotelSearch/HotelSearch.jsx
@@ -1,55 +1,84 @@
-import React, { useContext, useEffect } from "react";
-import NavBar from "../NavBar/newNavBar";
-import { makeStyles } from "@mui/styles";
+import React, { useState } from "react";
+import "./HotelSearch.css";
+import NavBar from "../../NavBar/newNavBar";
+// import { makeStyles } from "@mui/styles";
+import { FormControl } from "@mui/material";
+import { MdPlace } from "react-icons/md";
+import { BiSearch } from "react-icons/bi";
+import { cities } from "../../../db/cities";
+import axios from "axios";
+import { toast } from "react-toastify";
+import Select from "react-select";
+import { useHistory } from "react-router-dom";
 
-const useStyles = makeStyles({
-    root: {
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        width: "100vw",
-        backgroundColor: "#f5f5f5",
-    },
-    searchBox: {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        maxWidth: "500px",
-        height: "100%",
-        maxHeight: "300px",
+// const useStyles = makeStyles({});
 
-        "& input": {
-            width: "100%",
-            height: "50px",
-            padding: "0 20px",
-            border: "none",
-            outline: "none",
-            borderRadius: "5px 0 0 5px",
-            fontSize: "1.2rem",
-        },
-        "& button": {
-            width: "50px",
-            height: "50px",
-            border: "none",
-            outline: "none",
-            borderRadius: "0 5px 5px 0",
-            backgroundColor: "#f5f5f5",
-            cursor: "pointer",
-            "& svg": {
-                fontSize: "1.5rem",
-            },
-        },
-    },
-});
+export default function HotelSearch() {
+  const [city, setCity] = useState("");
+  // const classes = useStyles();
+  const history = useHistory();
+
+  const handleCity = (e) => {
+    setCity(e);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://188.121.113.74/api/hotel/search/", {
+        city: city.id,
+      })
+      .then((response) => {
+        console.log("the response of found hotels: ", response.data);
+        history.push("/found-hotels", { hotels: response.data });
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  return (
+    <div className="ContainerLandingPage">
+      <NavBar />
+      <form>
+        <div className="Title">
+          {/* <h2>راه حلی مناسب برای رزرو دکتر</h2> */}
+          <h1>دکترینو</h1>
+        </div>
+
+        <div className="SearchBar_Container">
+          <div className="chooseLocation">
+            <div className="LocationIcon">
+              <h2>مقصد</h2>
+              <MdPlace className="MdPlace" />
+            </div>
+            <div className="EnterCity">
+              <FormControl className="FormControlCity" fullWidth>
+                <Select
+                  value={city}
+                  onChange={(e) => handleCity(e)}
+                  options={cities}
+                  //   menuPortalTarget={document.body}
+                  //   styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+                />
+              </FormControl>
+            </div>
+          </div>
+          <div className="vl"></div>
+          <div className="searchScale">
+            <button onClick={handleSubmit} className="ZareBin">
+              {<BiSearch className="ZarebinIcon" />}
+            </button>
+          </div>
+        </div>
+
+        <div className="landingPage">
+          <div className="searchBar"></div>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 export default function HotelSearch() {
   const classes = useStyles();
