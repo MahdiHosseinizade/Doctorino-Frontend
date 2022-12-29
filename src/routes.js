f6302098b93de8d1fc7bc919c3ba194856a02d91
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2022-12-30 02:47:41 +0330

    Search Doctor Page-routes

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,19 @@
 // import doctorPanel from "./components/Doctor.jsx/doctorPanel";
 import Login from "./components/Login/Login";
 import SignUpPage from "./components/SignUp/SignUp";
-
 import HotelPanel from "./components/Hotel/Panel/HotelPanel";
 import Hotels from "./components/Hotel/HotelList/Hotels";
-
 import DoctorPanel from "./components/Doctor/DoctorPanel/DoctorPanel.jsx";
 import ListOfDoctors from "./components/Doctor/ListOfDoctors/ListOfDoctors";
 import DoctorProfileLayout from "./components/Doctor/DoctorProfile/DoctorProfileLayout";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import NotFound from "./pages/NotFoundPage";
-
 import scheduleTable from "./components/Doctor/DoctorProfile/scheduleTable";
 import drLayout from "./components/Doctor/DoctorProfile/DoctorProfileLayout";
 import Hotel from "./components/Hotel/Hotel/Hotel";
 import DoctorSpecialitie from "./components/Doctor/SpecialitiePage/DoctorSpecialities";
 import HotelReservation from "./components/Hotel/HotelReservation/HotelReservation";
+import SearchDoctorResponse from "./components/LandingPage/SearchDocResponse";
 
 const routes = [
   { path: "/schedule-table", component: scheduleTable },
@@ -26,7 +24,7 @@ const routes = [
   {path: "/specialist/:id", component: DoctorSpecialitie},
   { path: "/hotel-panel", component: HotelPanel, private: true },
   { path: "/hotels", component: Hotels },
-
+  {path: "/SearchDoctor" , component:SearchDoctorResponse },
   { path: "/doctor-panel", component: DoctorPanel, private: true },
   {path : "/hotel-reservation" , component: HotelReservation},
   { path: "/list-of-doctors", component: ListOfDoctors },
