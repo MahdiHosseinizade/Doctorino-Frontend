ae9a5552be65280b9a54b4acf7f5f60a4db9c17b
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-01-20 21:13:43 +0330

    add Pic and AboutUs

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,15 +1,27 @@
 import Profile from "./Profile";
 import './aboutus.css';
+import Seyed from '../../assets/img/MyPic.jpg';
+import Amad from '../../assets/img/Amad.jpg';
+import houman from '../../assets/img/houman.jpg'
+import SeyedMir from '../../assets/img/SeyedMir.jpg';
+import Mehti from '../../assets/img/Ghazavi.jpg';
+import NavBar from '../NavBar/newNavBar'
 const AboutUs = () => {
     return (
         <div>
             <div className="aboutUs">
-                <h1>About Us</h1>
+                {/* <h1>درباره ما</h1> */}
+                <NavBar/>
                 <br></br>
             </div>
             <div className="about-us">
-                <Profile />
-            </div>
+                <Profile name ={'سید مهدی حسینی زاده'} image={Seyed} position = {'فرانت اند'} />
+                <Profile name ={'احمد رضا طهماسبی'} image={Amad} position = {'بک اند'} />
+                <Profile name ={'رضا حقیقت گو'} image={houman} position = {'فرانت اند'} />
+                <Profile name ={'سید امیر حسین میر محمدی'} image={SeyedMir} position = {'بک اند'} />
+                <Profile name ={'مهدی قضاوی'} image={Mehti} position = {'فرانت اند'} />
+                <Profile name = {'محسن رحیمی'} image={Mehti} position = {'فرانت اند'} />
+            </div> 
         </div>
     );
 }
--- src/components/AboutUs/Profile.jsx
+++ src/components/AboutUs/Profile.jsx
@@ -1,15 +1,16 @@
 import './Profile.css';
 
-const Profile = () => {
+const Profile = ({name,image, position}) => {
     
     return (
         <div className="card">
             <div className="prof-image">
-                <img src="https://picsum.photos/200/300" alt="profile" />
+                <img src={image} alt="profile" />
             </div>
             <div className="insideText" >
-                <div className="insideText" >
-                    <h3>John Doe</h3>
+                <div className="cardTitle" >
+                    <h3 className='CardName'>{name}</h3>
+                    <h3 className='CardPosition'>توسعه دهنده {position}</h3>
                 </div>
             </div>
         </div>
